2e8a1d79b3476dc9cc9f11ca74fec6234cdf168d
TerryTian21 <terry.tian.2003@gmail.com>
2022-10-05 19:40:27 -0400

    POST to  Backend works

---
+++ mern/client/src/Components/get-user.component.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class GetUser extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Get Users</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+                />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
