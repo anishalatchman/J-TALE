a57a2b3bb4d734fd4f0ef39dc5fa5678c0b7a0ed
jkolyakov <jacobkolyakov@gmail.com>
2022-10-06 01:48:02 -0400

    voiceflow api post request

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('server'));
+app.use(express.static("server"));
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true});
+mongoose.connect(uri, { useNewUrlParser: true });
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
-
-const usersRouter = require('./routes/users');
-
-app.use('/users', usersRouter);
-
-app.listen(port, () => {
-console.log(`Server is running on port: ${port}`);
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
 
+const usersRouter = require("./routes/users");
+const transcriptRouter = require("./routes/transcript");
+
+app.use("/users", usersRouter);
+app.use("/transcript", transcriptRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
 
 // get driver connection
 // const dbo = require("./db/conn");
 
-
-  // perform a database connection when server starts
-  // dbo.connectToServer(function (err) {
-  //   if (err) console.error(err);
-  // });
+// perform a database connection when server starts
+// dbo.connectToServer(function (err) {
+//   if (err) console.error(err);
+// });
