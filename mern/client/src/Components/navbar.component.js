ddc3340e5295b50bd12f419a136b38427d08b115
TerryTian21 <terry.tian.2003@gmail.com>
2022-10-05 23:05:38 -0400

    Nodemon

--- mern/client/src/Components/navbar.component.js
+++ mern/client/src/Components/navbar.component.js
@@ -9,9 +9,9 @@ export default class Navbar extends Component {
           <Link to="/" className="navbar-brand">J TALE  </Link>
           <div className="collpase navbar-collapse">
           <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/get" className="nav-link">Get User</Link>
-          </li>
+            <li className="navbar-item">
+            <Link to="/get" className="nav-link">Get User</Link>
+            </li>
             <li className="navbar-item">
             <Link to="/user" className="nav-link">Create User</Link>
             </li>
