0a0ff9df0c1e46c2ebb36c2f2985a736bc401b92
jkolyakov <jacobkolyakov@gmail.com>
2022-10-19 21:24:59 -0400

    added generic buttons

--- mern/client/src/screens/Landing/landing.js
+++ mern/client/src/screens/Landing/landing.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./landing.css";
-
+import GenericButton from "../../Components/Buttons/GenericButton";
 export default class Landing extends Component {
   constructor(props) {
     super(props);
@@ -46,8 +46,18 @@ export default class Landing extends Component {
         </h1>
         <h4 className="subtitle">A flow-building plugin for Voiceflow</h4>
         <div className="buttonContainer">
-          <button className="button">Upload Transcript</button>
-          <button className="button">Recover Session</button>
+          <GenericButton
+            buttonType="blue"
+            onClick={() => null}
+            disabled={false}
+            text={"Upload Transcript"}
+          />
+          <GenericButton
+            buttonType="outline"
+            onClick={() => null}
+            disabled={false}
+            text={"Recover Session"}
+          />
         </div>
       </div>
     );
