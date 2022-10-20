a91c971c395319c0c3328037e34ca31eabe7cda0
jkolyakov <jacobkolyakov@gmail.com>
2022-10-19 22:11:35 -0400

    deleted excess code in landing.js

--- mern/client/src/screens/Landing/landing.js
+++ mern/client/src/screens/Landing/landing.js
@@ -3,41 +3,6 @@ import axios from "axios";
 import "./landing.css";
 import GenericButton from "../../Components/Buttons/GenericButton";
 export default class Landing extends Component {
-  constructor(props) {
-    super(props);
-
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
-    this.state = {
-      username: "",
-    };
-  }
-
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value,
-    });
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-
-    const user = {
-      username: this.state.username,
-    };
-
-    console.log(user);
-
-    axios
-      .post("http://localhost:5000/users/add", user)
-      .then((res) => console.log(res.data));
-
-    this.setState({
-      username: "",
-    });
-  }
-
   render() {
     return (
       <div className="container">
