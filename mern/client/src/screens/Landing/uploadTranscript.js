4c53635a92ed823a662ab092ad0ae3fb567ad375
jkolyakov <jacobkolyakov@gmail.com>
2022-10-27 00:30:46 -0400

    upload json files and read content via button click

--- mern/client/src/screens/Landing/uploadTranscript.js
+++ mern/client/src/screens/Landing/uploadTranscript.js
@@ -1,29 +1,63 @@
 import React, { Component } from "react";
 import "./uploadTranscript.css";
-import "./../../Components/Buttons/ButtonStyleSheet.css"
+import "./../../Components/Buttons/ButtonStyleSheet.css";
 import GenericButton from "../../Components/Buttons/GenericButton";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
 
 function UploadTranscript() {
   const Navigate = useNavigate();
   const PageChange = () => {
     Navigate("/");
-  }
+  };
+  const inputRef = useRef(null);
+  const [fileName, setFileName] = useState("No files chosen");
+  const [files, setFiles] = useState("");
+
+  const handleClick = () => {
+    // open file input box on click of button
+    inputRef.current.click();
+  };
+
+  //What happens when file is chosen
+  const handleFileChange = (event) => {
+    const fileObj = event.target.files && event.target.files[0];
+    console.log(typeof event.target.files[0]);
+    console.log(event.target.files[0]);
+    if (!fileObj) {
+      return;
+    }
+    // reset file input
+    event.target.value = null;
+    // Set display name of file
+    setFileName(fileObj.name);
+    const fileReader = new FileReader();
+    fileReader.readAsText(fileObj, "UTF-8");
+    fileReader.onload = (event) => {
+      setFiles(event.target.result);
+    };
+  };
   return (
     <div className="container">
-      <h1 className="h1 title">
-        Upload Transcripts
-      </h1>
+      <h1 className="h1 title">Upload Transcripts</h1>
       <div className="buttonContainer1">
-      <button className="button1">
-        <img
-        src={require("../../assets/uploadicon.png")}
-        alt={"upload"}
-        className="upload-icon"
+        <input
+          style={{ display: "none" }}
+          ref={inputRef}
+          type="file"
+          onChange={handleFileChange}
+          accept=".json"
         />
-        Choose File</button>
+        <button className="button1" onClick={handleClick}>
+          <img
+            src={require("../../assets/uploadicon.png")}
+            alt={"upload"}
+            className="upload-icon"
+          />
+          Choose File
+        </button>
       </div>
-      <h4 className="subtitle">No files chosen</h4>
+      <h4 className="subtitle"> {fileName + files} </h4>
       <div className="buttonContainer2">
         <GenericButton
           buttonType="blue"
@@ -33,7 +67,9 @@ function UploadTranscript() {
         />
         <GenericButton
           buttonType="outline"
-          onClick={() => {PageChange()}}
+          onClick={() => {
+            PageChange();
+          }}
           disabled={false}
           text={"Go Back"}
         />
@@ -41,35 +77,4 @@ function UploadTranscript() {
     </div>
   );
 }
-export default UploadTranscript
-
-// export default class Landing extends Component {
-    
-//     render() {
-//     return (
-//       <div className="container">
-//         <h1 className="h1 title">
-//           Upload Transcript
-//         </h1>
-//         <div className="buttonContainer">
-//         <button className="button1">Choose File</button>
-//         </div>
-//         <h4 className="subtitle">No file chosen</h4>
-//         <div className="buttonContainer">
-//         <GenericButton
-//             buttonType="outline"
-//             onClick={() => null}
-//             disabled={false}
-//             text={"Begin Session"}
-//           />
-//           <GenericButton
-//             buttonType="outline"
-//             onClick={() => null}
-//             disabled={false}
-//             text={"Go Back"}
-//           />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+export default UploadTranscript;
