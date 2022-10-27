e5e67c522b704121914bb6a3de3718277ee837e9
TerryTian21 <terry.tian.2003@gmail.com>
2022-10-26 21:00:20 -0400

    Revert "Merge pull request #41 from J-TALE/css_styling"
    This reverts commit 0a498523c330f83fd6548379db4fa9ddd9d87a59, reversing
    changes made to fc3dcaddf1b40f3c6f402c169dc798ef0ba34a0d.

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -7,7 +7,6 @@ import GetUser from "./Components/get-user.component";
 import CreateUser from "./Components/create-user.component";
 import GetTranscript from "./Components/get-transcript.component";
 import Landing from "./screens/Landing/landing";
-import UploadTranscript from "./screens/Landing/uploadTranscript"
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
           <Route path="/get" element={<GetUser />} />
           <Route path="/user" element={<CreateUser />} />
           <Route path="/transcript" element={<GetTranscript />} />
-          <Route path="/upload" element={<UploadTranscript />} />
         </Routes>
         {/* <button type="button" class="btn btn-primary btn-lg btn-block">J TALE is the best!</button> */}
       </div>
--- mern/client/src/screens/Landing/landing.js
+++ mern/client/src/screens/Landing/landing.js
@@ -1,70 +1,55 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./landing.css";
-import GenericButton from "../../Components/Buttons/GenericButton";
-import { Navigate, useNavigate } from "react-router-dom";
 
-function Landing() {
-  const Navigate = useNavigate();
-  const PageChange = () => {
-    Navigate("/upload");
+export default class Landing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+    };
   }
-  return (
-    <div className="container">
-      <h1 className="h1 title">
-        Transcript to chatbot <br /> with a couple clicks
-      </h1>
-      <h4 className="subtitle">A flow-building plugin for Voiceflow</h4>
-      <div className="buttonContainer">
-        <GenericButton
-          buttonType="blue"
-          onClick={() => {PageChange()}}
-          disabled={false}
-          text={"Upload Transcript"}
-        />
-        <GenericButton
-          buttonType="outline"
-          onClick={() => null}
-          disabled={false}
-          text={"Recover Session"}
-        />
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+    };
+
+    console.log(user);
+
+    axios
+      .post("http://localhost:5000/users/add", user)
+      .then((res) => console.log(res.data));
+
+    this.setState({
+      username: "",
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="h1 title">
+          Transcript to chatbot <br /> with a couple clicks
+        </h1>
+        <h4 className="subtitle">A flow-building plugin for Voiceflow</h4>
+        <div className="buttonContainer">
+          <button className="button">Upload Transcript</button>
+          <button className="button">Recover Session</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-export default Landing
-
-// export default class Landing extends Component {
-//   // constructor(props) {
-//   //   super(props)
-//   // }
-
-//   render() {
-//   function PageChange() {
-//     let Navigate = useNavigate();
-//       Navigate("/upload");
-//   }
-//     return (
-//       <div className="container">
-//         <h1 className="h1 title">
-//           Transcript to chatbot <br /> with a couple clicks
-//         </h1>
-//         <h4 className="subtitle">A flow-building plugin for Voiceflow</h4>
-//         <div className="buttonContainer">
-//           <GenericButton
-//             buttonType="blue"
-//             onClick={() => {console.log("hello"); PageChange()}}
-//             disabled={false}
-//             text={"Upload Transcript"}
-//           />
-//           <GenericButton
-//             buttonType="outline"
-//             onClick={() => null}
-//             disabled={false}
-//             text={"Recover Session"}
-//           />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
