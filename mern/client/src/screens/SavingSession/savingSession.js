9a24c0afd394e881f252da37b1ab05eee5f80fcf
Anisha Latchman <anishalatchman@gmail.com>
2022-11-08 11:52:44 -0500

    Added copy to clipboard functionality

--- mern/client/src/screens/SavingSession/savingSession.js
+++ mern/client/src/screens/SavingSession/savingSession.js
@@ -10,12 +10,27 @@ class SavingSession extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {value: ''};
+    this.state = {value: "17A540"};
     this.handleBack = this.handleBack.bind(this);
     };
 
+    setSessionID(id) {    
+      this.setState({value: id});
+    };
     handleBack(event) {
       this.props.navigate('/');
+    };
+    copyText(event) {
+      event.preventDefault();
+      // Get the text field
+      var textToCopy = document.getElementById("sessionID");
+
+      // Select the text field
+      textToCopy.select();
+      textToCopy.setSelectionRange(0, 99999); // For mobile devices
+
+      // Copy the text inside the text field
+      navigator.clipboard.writeText(textToCopy.value);
     };
  
 
@@ -24,13 +39,13 @@ class SavingSession extends Component {
       <div>
       <div className="container">
         <h1 className="pageTitle"> Session Saved </h1>
-        <h4 className="subTitle">You will need you session ID to continue next time.</h4>
+        <h4 className="subTitle">You will need your session ID to continue next time.</h4>
       <form className="inputForm w-1/3 mx-auto"> 
           <label className="text-xl text-white font-nunito font-medium"> Your Session ID </label>
-          <input className="m-6 w-3/4 pl-4 pr-2 pt-2 pb-2 block rounded-full text-black" type="text" value={this.state.value} onChange={this.handleBack} placeholder="Session ID" />        
+          <input className="m-6 w-3/4 pl-4 pr-2 pt-2 pb-2 text-center block rounded-full font-nunito" id="sessionID" type="text" readonly value={this.state.value}/>        
           <GenericButton
               buttonType="white"
-              onClick={() => this.handleBack}
+              onClick={this.copyText}
               disabled={false}
               text={"Copy to Clipboard"}
             />
