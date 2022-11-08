308778259c0b1a666829d5874af350b1d9078a52
Anisha Latchman <anishalatchman@gmail.com>
2022-11-07 20:30:48 -0500

    Add components to save session page

--- mern/client/src/screens/SavingSession/savingSession.js
+++ mern/client/src/screens/SavingSession/savingSession.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./savingSession.css";
 import GenericButton from "../../Components/Buttons/GenericButton";
 import { withRouter } from "../../withRouter";
+import { Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
@@ -10,48 +11,45 @@ class SavingSession extends Component {
     super(props);
 
     this.state = {value: ''};
-    this.handleHome = this.handleBack.bind(this);
-    // this.handleBack = this.handleBack.bind(this);
+    this.handleBack = this.handleBack.bind(this);
     };
 
-    handleHome(event) {
-        this.props.navigate('/') 
-      }
-
-    //   UNCOMMENT ONCE INTENT PAGE IS MERGED
-    // handleBack(event) {
-    //   this.props.navigate('/startingIntent') 
-    // }
+    handleBack(event) {
+      this.props.navigate('/');
+    };
+ 
 
   render() {
     return (
+      <div>
       <div className="container">
-        <h1 className="pageTitle">
-          Session Saved
-        </h1>
-      <form className="inputForm w-1/3 mx-auto" onSubmit={this.handleSubmit}> 
-          <label className="text-xl text-white font-nunito font-medium"> Input Session ID </label>
-          <input className="m-6 w-3/4 pl-4 pr-2 pt-2 pb-2 block rounded-full text-black" type="text" value={this.state.value} onChange={this.handleChange} placeholder="Session ID" />        
+        <h1 className="pageTitle"> Session Saved </h1>
+        <h4 className="subTitle">You will need you session ID to continue next time.</h4>
+      <form className="inputForm w-1/3 mx-auto"> 
+          <label className="text-xl text-white font-nunito font-medium"> Your Session ID </label>
+          <input className="m-6 w-3/4 pl-4 pr-2 pt-2 pb-2 block rounded-full text-black" type="text" value={this.state.value} onChange={this.handleBack} placeholder="Session ID" />        
           <GenericButton
               buttonType="white"
-              onClick={() => this.handleChange}
+              onClick={() => this.handleBack}
               disabled={false}
               text={"Copy to Clipboard"}
             />
       </form>
+      
       <div className="buttonRow">
       <GenericButton
               buttonType="blue"
-              onClick={this.handleHome}
+              onClick={() => null}
               disabled={false}
               text={"End Session"}
             />
         <GenericButton
               buttonType="outline"
-              onClick={() => null}
+              onClick={this.handleBack}
               disabled={false}
               text={"Go Back"}
             />
+      </div>
       </div>
       </div>
     );
@@ -59,4 +57,3 @@ class SavingSession extends Component {
 }
 
 export default withRouter(SavingSession);
-
