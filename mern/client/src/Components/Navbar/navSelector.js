9f76a4e1f7558a0ebd1ef6a8aa5965560cf0841b
Anisha Latchman <anishalatchman@gmail.com>
2022-11-13 01:46:05 -0500

    edited navSelector file

--- mern/client/src/Components/Navbar/navSelector.js
+++ mern/client/src/Components/Navbar/navSelector.js
@@ -1,9 +1,7 @@
 import React, { Component, useState } from "react";
-import { Link } from "react-router-dom";
 import "./navbar.css";
-import GenericButton from "../Buttons/GenericButton";
 
-export default function NavSelector({pageid}) {
+export default function NavSelector({navHome, navButtons }) {
   // React state to manage visibility
   const [show, setShow] = useState();
 
@@ -11,38 +9,10 @@ export default function NavSelector({pageid}) {
   function toggleShow() {
     setShow(!show);
   }
-  var buttonText = show ? "Hide Component" : "Show Component";
 
   return (
-    <nav className="navbar-bg">
-      <Link to="/" className="nav-link">
-        <div className="navbar-links">
-          <img
-            src={require("../../assets/voiceflow.png")}
-            alt={"voiceflow"}
-            className="nav-icon"
-          />
-          | J TALE
-        </div>
-      </Link>
-      <div className="flex items-center">
-        <h2 className="font-nunito font-medium">
-          SESSION ID: {sessionid}  
-          {/* NEED TO PASS IN SESSION ID VARIABLE HERE */}
-        </h2>
-        <GenericButton
-        buttonType="nav"
-        onClick={() => null}
-        disabled={false}
-        text={"SAVE"}
-      />
-        <GenericButton
-        buttonType="nav"
-        onClick={() => null}
-        disabled={false}
-        text={"DELETE SESSION"}
-      />
-      </div>
-    </nav>
+    <div>
+        {show ? navHome : navButtons}
+    </div>
   );
 }
