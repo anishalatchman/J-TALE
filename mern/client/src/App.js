5dda772af90b466a08e23858bf82f17c782ddd55
EmmaHkw <emmaho1125@gmail.com>
2022-11-19 15:08:57 -0500

    added provider in app.js

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -9,6 +9,7 @@ import UploadTranscript from "./screens/UploadTranscript/uploadTranscript";
 import StartingIntent from "./screens/StartingIntent/StartingIntent";
 import SavingSession from "./screens/SavingSession/savingSession";
 import SessionProvider from "./Contexts/sessionProvider";
+import IntentsProvider from "./Contexts/intentsProvider";
 
 function App() {
   return (
@@ -16,15 +17,17 @@ function App() {
       <div>
         {/* SessionProvider contains all global vars (navstate, session id) */}
         <SessionProvider>
-          <Navbar />
+          <IntentsProvider>
+            <Navbar />
 
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/recover" element={<RecoverSession />} />
-            <Route path="/upload" element={<UploadTranscript />} />
-            <Route path="/save" element={<SavingSession />} />
-            <Route path="/startingintent" element={<StartingIntent />} />
-          </Routes>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/recover" element={<RecoverSession />} />
+              <Route path="/upload" element={<UploadTranscript />} />
+              <Route path="/save" element={<SavingSession />} />
+              <Route path="/startingintent" element={<StartingIntent />} />
+            </Routes>
+          </IntentsProvider>
         </SessionProvider>
       </div>
     </Router>
