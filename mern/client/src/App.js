af364fc5a175b230d93f7bc410ae7c3900fc7c96
Anisha Latchman <anishalatchman@gmail.com>
2022-11-27 14:04:40 -0500

    Removed unused imports

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
       <div>
-        {/* SessionProvider contains all global vars (navstate, session id) */}
+        {/* SessionProvider contains all global vars (session id, transcript id) */}
         
         <SessionProvider>
 
