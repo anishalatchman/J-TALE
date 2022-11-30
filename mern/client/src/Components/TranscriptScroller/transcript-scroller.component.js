7e554ac0de0ec107d296ca887299044f33e10a54
Anisha Latchman <anishalatchman@gmail.com>
2022-11-30 13:26:04 -0500

    Moved transcript export button to scroller header

--- mern/client/src/Components/TranscriptScroller/transcript-scroller.component.js
+++ mern/client/src/Components/TranscriptScroller/transcript-scroller.component.js
@@ -6,8 +6,18 @@ export default class Scrollbar extends Component {
   render() {
     return (
       <div className="scroller">
-        <h1 className="scrollerTitle">Transcript</h1>
+        <div className="transcriptHeader">
+          <h1 className="scrollerTitle">Transcript</h1>
+          <GenericButton
+            buttonType="white"
+            // class="btn-bot"
+            onClick={() => null}
+            disabled={false}
+            text={"Export Transcript"}
+          />
+        </div >
 
+        <div className="transcriptText">
         <p>This is where the text of the transcript will be </p>
         <p>Bot: What can I help you with today?</p>
 
@@ -91,64 +101,8 @@ export default class Scrollbar extends Component {
           lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
           ipsum{" "}
         </p>
-        <p>
-          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum{" "}
-        </p>
-        <p>
-          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum{" "}
-        </p>
-        <p>
-          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum{" "}
-        </p>
-        <p>
-          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum{" "}
-        </p>
-        <p>
-          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum{" "}
-        </p>
-        <p>
-          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum{" "}
-        </p>
-        <p>
-          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum{" "}
-        </p>
-        <p>
-          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum{" "}
-        </p>
-
-        <div className="buttonContainer">
-          <GenericButton
-            buttonType="blue"
-            // class="btn-bot"
-            onClick={() => null}
-            disabled={false}
-            text={"Export Transcript"}
-          />
         </div>
+
       </div>
     );
   }
