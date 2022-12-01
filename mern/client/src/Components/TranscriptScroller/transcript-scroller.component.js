3da0482dcd8bda9f20e15257554862c902f16d4d
jkolyakov <jacobkolyakov@gmail.com>
2022-12-01 01:29:17 -0500

    fixed styling

--- mern/client/src/Components/TranscriptScroller/transcript-scroller.component.js
+++ mern/client/src/Components/TranscriptScroller/transcript-scroller.component.js
@@ -1,23 +1,23 @@
 import React, { useContext } from "react";
 import GenericButton from "../Buttons/GenericButton";
-import "./transcript-scroller.component.css";
+import styles from "./transcript-scroller.component.module.css";
 import { ScrollerContext } from "../../Contexts/scrollerProvider";
 
-export default function Scrollbar(props) {
+export default function Scrollbar() {
   const [speechList] = useContext(ScrollerContext);
   return (
-    <div className="scroller">
-      <div className="transcriptContainer">
-        <div className="transcriptHeader">
-          <h1 className="scrollerTitle">Transcript</h1>
+    <div className={styles.scroller}>
+      <div className={styles.transcriptContainer}>
+        <div className={styles.transcriptHeader}>
+          <h1 className={styles.scrollerTitle}>Transcript</h1>
         </div>
         {speechList.length === 0 ? (
           <></>
         ) : (
-          <div className="transcriptText">
+          <div className={styles.transcriptText}>
             {speechList.map((speech) => {
               return (
-                <p key={speech.text} className="bolded">
+                <p key={speech.text} className={styles.bolded}>
                   {speech.source + " " + speech.text}{" "}
                 </p>
               );
@@ -28,7 +28,7 @@ export default function Scrollbar(props) {
           </div>
         )}
       </div>
-      <div className="buttonContainer">
+      <div className={styles.buttonContainer}>
         <GenericButton
           buttonType="white"
           onClick={() => null}
