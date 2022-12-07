caae5ec31764144c419f6d79032f1456aff953da
Anisha Latchman <anishalatchman@gmail.com>
2022-12-06 21:15:44 -0500

    Remove unused import

--- mern/client/src/screens/StartingIntent/StartingIntent.js
+++ mern/client/src/screens/StartingIntent/StartingIntent.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import styles from "./StartingIntent.module.css";
 import "./../../Components/Buttons/ButtonStyleSheet.css";
 import GenericButton from "../../Components/Buttons/GenericButton";
